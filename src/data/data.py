271d192a0c958d0983227171d6f6e7a50ff1e7b7
yeonju7kim <yeonju7.kim@gmail.com>
2022-03-18 18:32:44 +0900

    CategoryDataset pickle파일로 저장

--- src/data/data.py
+++ src/data/data.py
@@ -1,5 +1,13 @@
-import torch
+import os.path
+
+import torch, pickle
 from torch.utils.data import DataLoader, Dataset
+
+'''
+
+'''
+
+category_dataset_pkl_file = '.cache/CategoryDataset.pkl'
 
 class CategoryDataset(Dataset):
     def __init__(self, sentence_list, label_list, transform, category_manager):
@@ -19,6 +27,21 @@ class CategoryDataset(Dataset):
     @property
     def transform(self):
         return self.transform
+
+    @staticmethod
+    def newCategoryDataset(sentence_list, label_list, transform, category_manager):
+        if os.path.exists(category_dataset_pkl_file):
+            print('load saved category dataset')
+            with open(category_dataset_pkl_file, 'rb') as f:
+                category_dataset = pickle.load(f)
+                return category_dataset
+        if os.path.exists('.cache') == False:
+            os.mkdir('.cache')
+        category_dataset = CategoryDataset(sentence_list, label_list, transform, category_manager)
+        with open(category_dataset_pkl_file, 'wb') as f:
+            print('save category dataset')
+            pickle.dump(category_dataset, f)
+        return category_dataset
 
 class Index:
     id_idx = 0
@@ -44,10 +67,16 @@ def read_txt_file(filename):
 
     return sentence_list, label_list
 
+def read_raw_txt_file(filename):
+    f = open(filename)
+    f.readline()
+    lines = f.readlines()
+
+    return lines
 
 def get_category_dataloader(batch_size, category_manager, transform, train_portion=0.7, shuffle=True, filename='data/1. 실습용자료.txt'): #, num_workers = 2):
     sentence_list, label_list = read_txt_file(filename)
-    category_dataset = CategoryDataset(sentence_list, label_list, transform, category_manager)
+    category_dataset = CategoryDataset.newCategoryDataset(sentence_list, label_list, transform, category_manager)
 
     dataset_size = len(category_dataset)
     train_size = (int)(train_portion * dataset_size)
@@ -62,6 +91,13 @@ def rawdata_to_sentence(rawdata):
     words = rawdata.split('|')
     return words[Index.text_obj_idx] + ' ' + words[Index.text_mthd_idx] + ' ' + words[Index.text_deal_idx]
 
+def fill_answer(sentence, big_category, mid_category, small_category):
+    sentence_list = sentence.split('|')
+    sentence_list.insert(1, small_category)
+    sentence_list.insert(1, mid_category)
+    sentence_list.insert(1, big_category)
+    sentence.join('|')
+    return sentence
 
 if __name__ == '__main__':
     tr_dataloader, val_dataloader = get_category_dataloader(10)
